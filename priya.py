384f3592322cdfcacdcda09ced97152c0819a5d2
Priya-549 <84494216+Priya-549@users.noreply.github.com>
2021-11-04 12:06:11 +0530

    Update priya.py

--- priya.py
+++ priya.py
@@ -6,9 +6,15 @@
 def swapcase(s):
     p=s.swapcase()
     print(p)
+def count(p):
     print(p[1])
+def reverse(r):
     reverse=p[::1]
     print(reverse)
 if __name__ == "__main__":
     s=input()
+    p=s.swapcase()
+    r=p[::1]
     swapcase(s)
+    count(p)
+    reverse(r)
