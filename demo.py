d274671b5648079db5c4bbb9b047b6d7942b8eb6
Priya-549 <84494216+Priya-549@users.noreply.github.com>
2021-11-04 11:12:43 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,11 +3,16 @@
 # pRIYA
 # R
 # AYIRp
-def fun():
-    return      
-s=input()
-p=s.swapcase()
-print(p)
-print(p[1])
-reverse=p[::-1]
-print(reverse)
+def swapcase():
+    return p 
+def count():
+    return print(p[1])
+def reverse():
+    return reverse
+if __name__ == "__main__":
+    s=input()
+    p=s.swapcase()
+    print(p)
+    print(p[1])
+    reverse=p[::-1]
+    print(reverse)
